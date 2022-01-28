1ea38afe4ee22e266510165bcf46b9433be4a390
adrianmarino <adrianmarino@gmail.com>
2022-01-28 19:42:03 -0300

    feat: add test cases descriptions

--- src/test/java/com/kata/ExperienceLevelUnitTest.java
+++ src/test/java/com/kata/ExperienceLevelUnitTest.java
@@ -1,6 +1,7 @@
 package com.kata;
 
 import com.kata.survivor.ExperienceLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.kata.survivor.ExperienceLevel.*;
@@ -10,27 +11,26 @@ import static org.hamcrest.Matchers.is;
 public class ExperienceLevelUnitTest {
 
     @Test
+    @DisplayName("When experience is between 0 and 6 it returns BLUE level")
     void test1() {
         assertThat(ExperienceLevel.from(3), is(BLUE));
     }
 
     @Test
+    @DisplayName("When experience is between 7 and 18 it returns BLUE level")
     void test2() {
         assertThat(ExperienceLevel.from(10), is(YELLOW));
     }
 
     @Test
+    @DisplayName("When experience is between 19 and 42 it returns YELLOW level")
     void test3() {
-        assertThat(ExperienceLevel.from(10), is(YELLOW));
-    }
-
-    @Test
-    void test4() {
         assertThat(ExperienceLevel.from(20), is(ORANGE));
     }
 
     @Test
-    void test5() {
+    @DisplayName("When experience is greater than 43 it returns RED level")
+    void test4() {
         assertThat(ExperienceLevel.from(60), is(RED));
     }
 }
